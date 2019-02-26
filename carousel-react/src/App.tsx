d29f30727757a57523ba87bd478dcc64a756ea9f
Bryan Gislason <brgisla@microsoft.com>
2019-02-26 11:52:00 -0800

    feat: connect to Flickr api

--- carousel-react/src/App.tsx
+++ carousel-react/src/App.tsx
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css';
-import Carousel from './components/carousel'
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import Carousel from "./components/carousel";
 
 class App extends Component<{}, any> {
   constructor(props: any) {
     super(props);
 
     this.state = {
-      images: [
-        'https://c1.staticflickr.com/6/5558/14816728310_fae96af284_b.jpg',
-        'https://c1.staticflickr.com/8/7281/8737514820_17c9e41d0f_h.jpg',
-        'https://c1.staticflickr.com/5/4826/44752608200_7111f63741_b.jpg',
-      ]
+      images: []
     };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d78588f066e433afc3fcabdf887c0ea7&safe_search=1&content_type=1&place_id=uiZgkRVTVrMaF2cP&media=photos&format=json&nojsoncallback=true",
+      {
+        method: "GET"
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        const imageSources = json.photos.photo.reduce(
+          (imageSources: string[], { farm, server, id, secret }: any) => {
+            return [
+              ...imageSources,
+              `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
+            ];
+          },
+          []
+        );
+
+        this.setState((state: any) => {
+          return {
+            ...state,
+            images: imageSources
+          };
+        });
+      });
   }
   render() {
     return (
       <div className={styles.App}>
         <header className={styles.AppHeader}>
-          <Carousel imageSources={this.state.images}/>
+          <Carousel imageSources={this.state.images} />
         </header>
       </div>
     );
