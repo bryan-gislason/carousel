bbdf9fa0ab7345ee9fff385c421c74431cacb551
Bryan Gislason <brgisla@microsoft.com>
2019-02-26 10:42:13 -0800

    feat: add left and right arrow button functionality to switch between images

--- carousel-react/src/App.tsx
+++ carousel-react/src/App.tsx
@@ -3,12 +3,24 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 import Carousel from './components/carousel'
 
-class App extends Component {
+
+class App extends Component<{}, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      images: [
+        'https://c1.staticflickr.com/6/5558/14816728310_fae96af284_b.jpg',
+        'https://c1.staticflickr.com/8/7281/8737514820_17c9e41d0f_h.jpg',
+        'https://c1.staticflickr.com/5/4826/44752608200_7111f63741_b.jpg',
+      ]
+    };
+  }
   render() {
     return (
       <div className={styles.App}>
         <header className={styles.AppHeader}>
-          <Carousel />
+          <Carousel imageSources={this.state.images}/>
         </header>
       </div>
     );
--- carousel-react/src/components/carousel/carousel.tsx
+++ carousel-react/src/components/carousel/carousel.tsx
@@ -2,23 +2,64 @@ import React from "react";
 import styles from "./carousel.module.css";
 import { LeftArrow, RightArrow } from "../ui-core/icon";
 
-interface CarouselProps {}
-class Carousel extends React.Component<CarouselProps, {}> {
+interface CarouselState {
+  readonly currentImageIndex: number;
+}
+
+interface CarouselProps {
+  imageSources: string[];
+}
+class Carousel extends React.Component<CarouselProps, CarouselState> {
   constructor(props: CarouselProps) {
     super(props);
+
+    this.state = {
+      currentImageIndex: 0
+    };
+
+    this.onRightClick = this.onRightClick.bind(this);
+    this.onLeftClick = this.onLeftClick.bind(this);
   }
-  render() {
+
+  private onRightClick() {
+    this.setState((state, props) => {
+      return {
+        ...state,
+        currentImageIndex:
+          (state.currentImageIndex + 1) % props.imageSources.length
+      };
+    });
+  }
+
+  private onLeftClick() {
+    this.setState((state, props) => {
+      const isNeg = state.currentImageIndex - 1 < 0;
+      return {
+        ...state,
+        currentImageIndex: isNeg
+          ? props.imageSources.length - 1
+          : state.currentImageIndex - 1
+      };
+    });
+  }
+
+  public render() {
+    const { imageSources } = this.props;
+    const imageSource = imageSources[this.state.currentImageIndex];
     return (
       <div className={styles.carousel}>
         <div className={styles.body}>
-          <button className={`${styles.button} ${styles.buttonLeft}`}>
+          <button
+            className={`${styles.button} ${styles.buttonLeft}`}
+            onClick={this.onLeftClick}
+          >
             <LeftArrow />
           </button>
-          <img
-            className={styles.image}
-            src="https://c1.staticflickr.com/6/5558/14816728310_fae96af284_b.jpg"
-          />
-          <button className={`${styles.button} ${styles.buttonRight}`}>
+          <img className={styles.image} src={imageSource} />
+          <button
+            className={`${styles.button} ${styles.buttonRight}`}
+            onClick={this.onRightClick}
+          >
             <RightArrow />
           </button>
         </div>
